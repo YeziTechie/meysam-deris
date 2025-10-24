e7e16feb22e05914acc644096c4909cc9bb2bd98
YeziTechie <yazdan.hka@gmail.com>
2025-10-24 13:19:03 +0330

    files almost added, some need to be clearified, scroll video updated and added

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-const section = document.querySelector('.video-container');
-const vid = document.querySelector('.js-video');
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-vid.pause();
+const vid = document.querySelector(".js-video");
 
-let targetTime = 0;
-let currentTime = 0;
+vid.addEventListener("loadedmetadata", () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-function scroll() {
-  const distance = window.scrollY - section.offsetTop;
-  const total = section.clientHeight - window.innerHeight;
-
-  let percentage = distance / total;
-  percentage = Math.max(0, Math.min(percentage, 1));
-
-  if (vid.duration > 0) {
-    targetTime = vid.duration * percentage;
-  }
-}
-
-// smooth interpolation loop
-function smoothUpdate() {
-  if (vid.duration > 0) {
-    currentTime += (targetTime - currentTime) * 0.1; // 0.1 = smoothness factor
-    vid.currentTime = currentTime;
-  }
-  requestAnimationFrame(smoothUpdate);
-}
-
-window.addEventListener('scroll', scroll);
-scroll();
-smoothUpdate();
+  gsap.to(vid, {
+    currentTime: vid.duration,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".video-container",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+    },
+  });
+});
