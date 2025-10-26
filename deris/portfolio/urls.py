1be9be9cc56dd79d7292578bb382c94223c1d55e
YeziTechie <yazdan.hka@gmail.com>
2025-10-26 17:40:12 +0330

    all three pages added

--- deris/portfolio/urls.py
+++ deris/portfolio/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from portfolio import views
 
 urlpatterns = [
-    path('', views.profile_page, name='profile_page'),
-    path('photography/', views.photography_list, name='photography_list'),
-    path('videography/', views.videography_list, name='videography_list'),
-    path('ai/', views.ai_list, name='ai_list'),
+    path('', views.profile_page, name='profile'),
+    path('photography/', views.photography_list, name='photography'),
+    path('videography/', views.videography_list, name='videography'),
+    path('ai/', views.ai_list, name='ai'),
     path('contact/', views.contact_form, name='contact_form'),
 ]
